0bbeb07643647235fddad33e119e6b097b260c4c
blake williamson <miya.ashbrook@gmail.com>
2021-12-25 00:00:26 -0800

    Delete text.h

--- text.h
+++
@@ -1,9 +0,0 @@
-#ifndef TEXT_DEF
-#define TEXT_DEF
-
-
-void Text_Init(void);
-void Text_Draw(int x, int y, int hSpacing, int vSpacing, int maxWidth, const char *text);
-void Text_Close(void);
-
-#endif
